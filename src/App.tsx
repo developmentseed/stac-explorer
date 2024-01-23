47a9cffcb0b7f8d96365ecde62ebc9d9d955ce34
Oliver Roick <oliver@roick.email>
2024-01-23 15:17:55 +1100

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import {
   ChakraProvider,
   Box,
   Grid,
   GridItem,
+  Text
 } from "@chakra-ui/react";
 import theme from "./theme";
 import DataSelector from './components/DataSelector';
@@ -18,6 +18,7 @@ export default function App() {
         h="100vh"
       >
         <GridItem>
+          <Text as="h1" fontSize="large" fontWeight="bold" textTransform="uppercase" borderBottom="1px solid" borderColor="gray.100" mb="4" pb="4">STAC Explorer</Text>
           <DataSelector />
           <Box>other selections</Box>
         </GridItem>
