a45154869bd68f4d98a8c5a85fc901d40532b809
Oliver Roick <oliver@roick.email>
2024-03-19 17:15:08 +1100

    Add date picker

--- src/components/LayerList/HlsLayerForm.tsx
+++ src/components/LayerList/HlsLayerForm.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { LayerConfig, StacCollection } from "../../types";
 import { FormControl, FormLabel } from "@chakra-ui/react";
-import DateRangeSlider from "../generic/DateRangeSlider";
+// import DateRangeSlider from "../generic/DateRangeSlider";
+import { RangeDatepicker } from "chakra-dayzed-datepicker";
+import { durationToMs } from "../../utils";
 
 type Props = {
   config: LayerConfig;
@@ -15,27 +17,59 @@ function HlsLayerForm({ config, collection, updateLayer }: Props) {
 
   const [ selectedRange, setSelectedRange ] = useState<string | undefined>(config.renderConfig.datetime);
 
-  const submit = () => {
-    updateLayer({
-      ...config,
-      renderConfig: {
-        ...config.renderConfig,
-        datetime: selectedRange
-      }
-    })
-  }
+  const [minDate, maxDate] = useMemo(() => {
+    const extentMinMs = timeMin ? Date.parse(timeMin) : 0;
+    const extentMaxMs = timeMax ? Date.parse(timeMax) : Date.now();
+
+    let minMs = extentMinMs;
+    let maxMs = extentMaxMs;
+
+    const selectedDates = selectedRange ? selectedRange.split('/') : [];
+    if (selectedDates.length === 1) {
+      const interval = datetime_range ? durationToMs(datetime_range[1]) : 0;
+      minMs = interval > 0 ? Date.parse(selectedDates[0]) - interval : minMs;
+      maxMs = interval > 0 ? Date.parse(selectedDates[0]) + interval : maxMs;
+    }
+
+    return [new Date(minMs), new Date(maxMs)];
+  }, [datetime_range, selectedRange, timeMax, timeMin])
+
+  const selectedDates = useMemo(() => {
+    if (!selectedRange) {
+      return [minDate, maxDate];
+    }
+    const dates = selectedRange.split('/')
+      .filter((d) => d !== '..')
+      .map((d) => new Date(Date.parse(d)));
+    return dates;
+  }, [maxDate, minDate, selectedRange]);
+
+  const onChange = useCallback((dates: Date[]) => {
+    setSelectedRange(dates.map((d) => d.toISOString()).join('/'));
+  }, []);
+
+  useEffect(() => {
+    if(selectedRange) {
+      updateLayer({
+        ...config,
+        renderConfig: {
+          ...config.renderConfig,
+          datetime: selectedRange
+        }
+      });
+    }
+  }, [config, selectedRange, updateLayer]);
+
 
   return (
     <FormControl>
       <FormLabel as="div">Date range</FormLabel>
-      <DateRangeSlider
-        min={timeMin}
-        max={timeMax}
-        aria-label="Date range"
-        value={selectedRange}
-        rangeBoundary={datetime_range}
-        onChange={setSelectedRange}
-        onChangeEnd={submit}
+      <RangeDatepicker
+        selectedDates={selectedDates}
+        onDateChange={onChange}
+        minDate={minDate}
+        maxDate={maxDate}
+        usePortal
       />
     </FormControl>
   );
